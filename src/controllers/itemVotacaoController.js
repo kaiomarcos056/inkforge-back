410c2c016ecd9b7ed815eb48accced8408b5e9d1
Marcos Kaio <kaionery111@gmail.com>
2025-02-24 11:35:16 -0300

    lista item votacao

--- src/controllers/itemVotacaoController.js
+++ src/controllers/itemVotacaoController.js
@@ -23,7 +23,15 @@ const listarItemPorVotacao = async (req, res) => {
         console.log(uuid_votacao)
         const { rows } = await pool.query(
             `
-            select * from item_votacao where uuid_votacao = $1
+            SELECT 
+                I.*
+            FROM ITEM_VOTACAO I
+            INNER JOIN VOTACAO V ON V.UUID_VOTACAO = I.UUID_VOTACAO
+            INNER JOIN CAPITULO C ON C.UUID_CAPITULO = V.UUID_CAPITULO
+            INNER JOIN LIVRO L ON L.UUID_LIVRO = C.UUID_LIVRO
+            INNER JOIN USUARIO U ON U.UUID_USUARIO = I.UUID_USUARIO
+            WHERE L.UUID_USUARIO = I.UUID_USUARIO
+            AND I.UUID_VOTACAO = $1
             `,
             [uuid_votacao]
         );
